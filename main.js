fc618dba9a155f76a954cac82142fc1fbd1a199a
mahfuj890 <mahfujali490@gmail.com>
2022-02-23 02:41:21 +0600

    add logic number

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log(document.querySelector(".message"));
+
+//Get the random value
+const randomNumber = Math.trunc(Math.random()* 20) + 1 ;
+document.querySelector(".number").textContent = randomNumber;
+
+//Click Event
+ let checkButton = document.querySelector(".check");
+ if(checkButton){
+     checkButton.addEventListener("click",()=>{
+         let guessValue = Number(document.querySelector(".guess").value) ;
+         let messageText =  document.querySelector(".message");
+         if(!guessValue){
+           messageText.textContent="⚙ No Number";
+         }
+         else if(guessValue < 0 ){
+             alert("Please Enter A Positive Number!!")
+             guessValue.value="";
+         }
+         else if(guessValue === randomNumber){
+            messageText.textContent="🔓 Correct Number";
+         }
+         else if(guessValue > randomNumber){
+            messageText.textContent="🔐 Too High";
+         }
+         else if(guessValue < randomNumber){
+            messageText.textContent="🔐 Too Low";
+         }
+     })
+ }

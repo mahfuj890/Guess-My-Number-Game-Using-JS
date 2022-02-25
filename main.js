574a0af7c0dc1a156f7be5e1b3d434d68b732fe9
mahfuj890 <mahfujali490@gmail.com>
2022-02-25 15:36:10 +0600

    add again button funcationlity

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 "use strict";
 
 //Get the random value
-const randomNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = randomNumber;
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let scoreValue = 20;
+let numberValue = document.querySelector(".number")
+let messageText = document.querySelector(".message");
+let score = document.querySelector(".score");
+numberValue.textContent = randomNumber;
 //Click Event
 let checkButton = document.querySelector(".check");
 if (checkButton) {
   checkButton.addEventListener("click", () => {
     let guessValue = Number(document.querySelector(".guess").value);
-    let messageText = document.querySelector(".message");
-    let score = document.querySelector(".score");
+
     if (!guessValue) {
       messageText.textContent = "⚙ No Number";
     } else if (guessValue < 0) {
@@ -18,6 +20,10 @@ if (checkButton) {
       guessValue.value = "";
     } else if (guessValue === randomNumber) {
       messageText.textContent = "🔓 Correct Number";
+      document.querySelector("body").style.cssText =
+        "background-color: #60b347;";
+      document.querySelector(".number").style.cssText =
+        "width:30rem;color:white;background: #f30f0f ";
     } else if (guessValue > randomNumber) {
       if (scoreValue > 1) {
         messageText.textContent = "🔐 Too High";
@@ -28,14 +34,28 @@ if (checkButton) {
         score.textContent = 0;
       }
     } else if (guessValue < randomNumber) {
-        if (scoreValue > 1) {
-            messageText.textContent = "🔐 Too Low"
-            scoreValue--;
-            score.textContent = scoreValue;
-          } else {
-            messageText.textContent = "🔏 You Lose The Game";
-            score.textContent = 0;
-          }
+      if (scoreValue > 1) {
+        messageText.textContent = "🔐 Too Low";
+        scoreValue--;
+        score.textContent = scoreValue;
+      } else {
+        messageText.textContent = "🔏 You Lose The Game";
+        score.textContent = 0;
+      }
     }
   });
 }
+
+document.querySelector(".again").addEventListener("click",()=>{
+  scoreValue = 20;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+ console.log(randomNumber,scoreValue);
+ messageText.textContent="Start Guessing....";
+ score.textContent = scoreValue
+ numberValue.textContent="?"
+ document.querySelector(".guess").value="";
+ document.querySelector("body").style.cssText =
+ "background-color: #222;";
+document.querySelector(".number").style.cssText =
+ "width:15rem;";
+})

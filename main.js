f5256371f62ba1b2169df5a5dc34dfbc7651943b
mahfuj890 <mahfujali490@gmail.com>
2022-02-27 10:51:17 +0600

    store highscore funcatility

--- main.js
+++ main.js
@@ -3,16 +3,19 @@
 //Get the random value
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let scoreValue = 20;
-let numberValue = document.querySelector(".number")
+let highScore = 0;
+let numberValue = document.querySelector(".number");
 let messageText = document.querySelector(".message");
 let score = document.querySelector(".score");
-numberValue.textContent = randomNumber;
-//Click Event
+let devButton = document.querySelector(".devButton");
+numberValue.textContent = "?";
 let checkButton = document.querySelector(".check");
+
+
+//Click Event
 if (checkButton) {
   checkButton.addEventListener("click", () => {
     let guessValue = Number(document.querySelector(".guess").value);
-
     if (!guessValue) {
       messageText.textContent = "⚙ No Number";
     } else if (guessValue < 0) {
@@ -24,6 +27,8 @@ if (checkButton) {
         "background-color: #60b347;";
       document.querySelector(".number").style.cssText =
         "width:30rem;color:white;background: #f30f0f ";
+      highScore += scoreValue;
+      document.querySelector(".highscore").textContent = highScore;
     } else if (guessValue > randomNumber) {
       if (scoreValue > 1) {
         messageText.textContent = "🔐 Too High";
@@ -46,16 +51,23 @@ if (checkButton) {
   });
 }
 
-document.querySelector(".again").addEventListener("click",()=>{
+//Run Radmon Number Again Without Page Reload
+document.querySelector(".again").addEventListener("click", () => {
   scoreValue = 20;
   randomNumber = Math.trunc(Math.random() * 20) + 1;
- console.log(randomNumber,scoreValue);
- messageText.textContent="Start Guessing....";
- score.textContent = scoreValue
- numberValue.textContent="?"
- document.querySelector(".guess").value="";
- document.querySelector("body").style.cssText =
- "background-color: #222;";
-document.querySelector(".number").style.cssText =
- "width:15rem;";
-})
+  console.log(randomNumber, scoreValue);
+  messageText.textContent = "Start Guessing....";
+  score.textContent = scoreValue;
+  numberValue.textContent = randomNumber;
+  numberValue.textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.cssText = "background-color: #222;";
+  document.querySelector(".number").style.cssText = "width:15rem;";
+});
+
+//Display Number For Developer
+if (devButton) {
+  devButton.addEventListener("click", () => {
+    numberValue.textContent = randomNumber;
+  });
+}
